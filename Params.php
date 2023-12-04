d13e326f77993b324842997fb318d4538b2d8177
EdsonZuchi <edsonluiz.zuchi@gmail.com>
2023-12-03 21:49:19 -0300

    ...

--- Params.php
+++ Params.php
@@ -1,7 +1,7 @@
 <?php
-    define("DB_HOST", "localhost");
-    define("DB_PORT", 3306);
-    define("DB_NAME", "cepdb");
-    define("DB_USERNAME", "root");
-    define("DB_PASSWORD", "root");
+    define("DB_HOST", $_ENV['MYSQL_URL']);
+    define("DB_PORT", $_ENV['MYSQLPORT']);
+    define("DB_NAME", $_ENV['MYSQL_DATABASE']);
+    define("DB_USERNAME", $_ENV['MYSQLUSER']);
+    define("DB_PASSWORD", $_ENV['MYSQLPASSWORD']);
 ?>
